bfa18eae8a03787cdebeaaee038e78230d2f4f20
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-12 20:26:58 +0100

    main-content

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,18 +1,66 @@
 @extends('layout')
 
-@section('content')
+@section('main-content')
+  <div class="main-background">
 
-  @foreach($pastaArray as $pasta)
-    <div class="pasta">
-      @php
-        $link = "/product/" . $pasta["titolo"];
-      @endphp
-      <a href="{{$link}}"> {{$pasta["titolo"]}} </a>
-      {{$pasta["tipo"]}} <br>
-      {{$pasta["cottura"]}} <br>
+    <div class="main-content">
+      <div class="main-content-section">
+        <h2>LE LUNGHE</h2>
+        @foreach($pastaArray as $pasta)
+          @if($pasta["tipo"]==="lunga")
+            <div class="pasta">
+              {{-- Link pagina prodotto --}}
+              @php
+                $link = "/product/" . $pasta["titolo"];
+              @endphp
 
-      <br> <br>
+              <a href="{{$link}}">
+                {{-- Immagine --}}
+                <img class="pasta-img"src="{{$pasta["src"]}}" alt="" >
+              </a>
+
+            </div>
+          @endif
+        @endforeach
+      </div>
+      <div class="main-content-section">
+        <h2>LE CORTE</h2>
+        @foreach($pastaArray as $pasta)
+          @if($pasta["tipo"]==="corta")
+            <div class="pasta">
+              {{-- Link pagina prodotto --}}
+              @php
+                $link = "/product/" . $pasta["titolo"];
+              @endphp
+
+              <a href="{{$link}}">
+                {{-- Immagine --}}
+                <img class="pasta-img"src="{{$pasta["src"]}}" alt="" >
+              </a>
+
+            </div>
+          @endif
+        @endforeach
+      </div>
+      <div class="main-content-section">
+        <h2>LE CORTISSIME</h2>
+        @foreach($pastaArray as $pasta)
+          @if($pasta["tipo"]==="cortissima")
+            <div class="pasta">
+              {{-- Link pagina prodotto --}}
+              @php
+                $link = "/product/" . $pasta["titolo"];
+              @endphp
+
+              <a href="{{$link}}">
+                {{-- Immagine --}}
+                <img class="pasta-img"src="{{$pasta["src"]}}" alt="" >
+              </a>
+
+            </div>
+          @endif
+        @endforeach
+      </div>
     </div>
-
-  @endforeach
+  </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
 
+    <!-- Inizializzazione larghezza device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     {{-- File css --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
@@ -12,11 +15,25 @@
     {{-- CONTAINER --}}
     <div class="container">
       <header>
-        Ciao sono header!
+        <div class="header-background">
+          <div class="header-content">
+            <img class="header-content-logo" src="{{ asset('img/logo.png') }}" alt="tag">
+              <ul class="header-content-menu">
+                <li>Home</li>
+                <li>Prodotti</li>
+                <li>News</li>
+              </ul>
+          </div>
+
+        </div>
+
+
       </header>
 
-      {{-- CONTENT --}}
-      @yield('content')
+      {{-- MAIN-CONTENT --}}
+      @yield('main-content')
+
+
 
       <footer>
         Ciao sono footer!
